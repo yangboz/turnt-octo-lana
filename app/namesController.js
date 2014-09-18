08c1ff72c66643e7763dcedfb53ae3e6929f01d9
yangboz <YoungWelle@gmail.com>
2014-09-18 14:29:19 +0800

    ADDED:angular app example with standalone controller file and filter;

---
+++ app/namesController.js
@@ -0,0 +1,10 @@
+/**
+ * Created by yangboz on 14-9-18.
+ */
+function namesController($scope){
+    $scope.names = [
+        {name:'Jani',country:'Norway'},
+        {name:'Hege',country:'Sweden'},
+        {name:'Kai',country:'Denmark'}
+    ];
+}
